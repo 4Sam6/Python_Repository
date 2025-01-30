7e5e619c49aaf18ab8c90ea30f4aca03980a0692
Sam Hunt <hunts5724@gmail.com>
2025-01-30 16:52:35 +0000

    added a description to the start

--- bar2.py
+++ bar2.py
@@ -1,3 +1,8 @@
+#This will create a graph that has a bar for the male totals and a bar for the female totals
+
+#This version didnt have a copy so description so I have now included one
+
+
 import os
 import pandas as pd
 import requests
@@ -78,6 +83,6 @@ plt.title('Totals of Gender')
 # Show the plot
 plt.xticks(rotation=0)
 plt.show()
-
+#
 
 plt.savefig(f"{OUTPUT_DIR}/bird__total__bar{current_date}.png")# This OUTPUT_DIR was made for use with .gitingore 
