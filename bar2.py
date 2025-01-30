293ff56837530efe41b4105a0c0637b03b33baed
Sam Hunt <hunts5724@gmail.com>
2025-01-30 14:37:39 +0000

    practise commit

--- bar2.py
+++ bar2.py
@@ -1,3 +1,6 @@
+# this produces a simple graph that shows all the the results of the survey 
+
+
 import os
 import pandas as pd
 import requests
@@ -80,4 +83,4 @@ plt.xticks(rotation=0)
 plt.show()
 
 
-plt.savefig(f"{OUTPUT_DIR}/birdtotalbar{current_date}.png")# This OUTPUT_DIR was made for use with .gitingore 
+plt.savefig(f"{OUTPUT_DIR}/bird_total_bar{current_date}.png")# This OUTPUT_DIR was made for use with .gitingore 
